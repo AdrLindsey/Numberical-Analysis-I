2b8b630b0d5a19c607e1a5dd0893c9ede49dabe1
Adrian Lindsey <adrlindsey@gmail.com>
2021-04-13 14:54:27 -0400

    Delete we directory

--- Working-Examples/1DMethods/trapezoid.m
+++
@@ -1,9 +0,0 @@
-function y = trapezoid(f, a, b, n)
-  % This function implements the trapezoidal rule.  Integration
-  % is performed over n points on the interval a <= x < b
-
-  h = (b-a)/n;     % Step size
-  x = a:h:b;   % Sample x values
-  s = f(x);
-  y = h*( s(1) + 2*sum( s(2:(end-1)) ) + s(end) )/2;
-end
